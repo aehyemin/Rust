6bbdaeec28176dbc0a43783490a00b9860879b33
aehyemin <hsy13467926@gmail.com>
2024-04-04 22:56:30 +0900

    cli_tutorial

---
+++ cli/target/debug/cli.d
@@ -0,0 +1,1 @@
+C:\Users\ha\Desktop\rustoooo\rust1\cli\target\debug\cli.exe: C:\Users\ha\Desktop\rustoooo\rust1\cli\src\main.rs
---
+++ cli/target/debug/deps/cli.d
@@ -0,0 +1,5 @@
+C:\Users\ha\Desktop\rustoooo\rust1\cli\target\debug\deps\cli.exe: src\main.rs
+
+C:\Users\ha\Desktop\rustoooo\rust1\cli\target\debug\deps\cli.d: src\main.rs
+
+src\main.rs:
